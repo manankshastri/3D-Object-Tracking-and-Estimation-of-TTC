017143b961466e7e8cb4c9417887232eab0813dc
Manank Shastri <manank.shastri@gmail.com>
2020-04-29 03:11:57 -0400

    added kdtree implementation for lidar ttc

--- src/camFusion.hpp
+++ src/camFusion.hpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <opencv2/core.hpp>
 #include "dataStructures.h"
+#include "kdtree.h"
 
 
 void clusterLidarWithROI(std::vector<BoundingBox> &boundingBoxes, std::vector<LidarPoint> &lidarPoints, float shrinkFactor, cv::Mat &P_rect_xx, cv::Mat &R_rect_xx, cv::Mat &RT);
@@ -16,6 +17,18 @@ void show3DObjects(std::vector<BoundingBox> &boundingBoxes, cv::Size worldSize, 
 
 void computeTTCCamera(std::vector<cv::KeyPoint> &kptsPrev, std::vector<cv::KeyPoint> &kptsCurr,
                       std::vector<cv::DMatch> kptMatches, double frameRate, double &TTC, cv::Mat *visImg=nullptr);
+
+// void clusterHelper(int index, const std::vector<std::vector<float>>& points, std::vector<int>& cluster, std::vector<bool>& processed,
+//         const std::shared_ptr<KdTree>& tree, float distanceTol);
+
+// std::vector<std::vector<int>> euclideanCluster(const std::vector<std::vector<float>>& points, const std::shared_ptr<KdTree>& tree, float distanceTol);
+
+// std::vector<LidarPoint> removeOutliers(const std::vector<LidarPoint>& lidarPoints, float distanceTol);
+
+void clusterHelper(int indice, const std::vector<std::vector<float>> points, std::vector<int> &cluster, std::vector<bool> &processed, KdTree* tree, float distanceTol);
+
+std::vector<std::vector<int>> euclideanCluster(const std::vector<std::vector<float>>& points, KdTree* tree, float distanceTol);
+
 void computeTTCLidar(std::vector<LidarPoint> &lidarPointsPrev,
                      std::vector<LidarPoint> &lidarPointsCurr, double frameRate, double &TTC);                  
 #endif /* camFusion_hpp */
